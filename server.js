69e5cb36db1784ea4bcc5b32a63bbdd4d2faa8d0
Jon <jonathan.divi@gmail.com>
2022-07-26 21:34:17 -0400

    added files and folders

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //---- Requirements
 const express = require('express')
+const { Db } = require('mongodb')
 const app = express()
 const MongoClient=require('mongodb').MongoClient
 const PORT = 8001
@@ -7,22 +8,43 @@ require('dotenv').config()
 
 
 //---- Database connection
+let db,
+    dbConnectionStr=process.env.DB_STRING,
+    dbName='todo'
 
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology:true})
+    .then(client=>{
+        console.log(`Connected to ${dbName} Database`)
+        db=client.db(dbName)
+    })
 
 //---- Modules
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
 //---- Routes
-    //---- Get
+    //---- Receive (Pulls in data from our database)
+    app.get
+
+    //---- Create (Adds something to our database)
+    app.post
 
 
-    //---- Post
+    //---- Update (updates something in our database)
+    app.put
 
 
-    //---- Put
+    app.put
 
 
-    //---- Delete
+    //---- Delete (deletes something from our database)
+    app.delete
 
 
 //---- Port Listener
+app.listen(PORT, ()=>{
+    console.log(`The todo-list server is running on port ${PORT}`)
+})
