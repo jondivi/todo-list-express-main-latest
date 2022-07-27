7ecd4fff38deb4e8ff7bb024cfcde3d49b087566
Jon <jonathan.divi@gmail.com>
2022-07-26 21:04:00 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+//---- Requirements
+const express = require('express')
+const app = express()
+const MongoClient=require('mongodb').MongoClient
+const PORT = 8001
+require('dotenv').config()
+
+
+//---- Database connection
+
+
+//---- Modules
+
+
+//---- Routes
+    //---- Get
+
+
+    //---- Post
+
+
+    //---- Put
+
+
+    //---- Delete
+
+
+//---- Port Listener
